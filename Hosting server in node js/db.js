6708973cbd40e1e8d14a05c38c29366ef0d280a0
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-07-03 22:45:59 +0600

    shift mongodb to online server

--- Hosting server in node js/db.js
+++ Hosting server in node js/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define the MongoDB Connection URL
-const mongoURL = 'mongodb://localhost:27017/hotels'
+// const mongoURL = 'mongodb://localhost:27017/hotels'
+// const mongoURL = 'mongodb+srv://helloworld:qwerty123@cluster0.htezcpk.mongodb.net/'
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB Server
 // mongoose.connect(mongoURL, {
@@ -11,6 +14,10 @@ const mongoURL = 'mongodb://localhost:27017/hotels'
 
 mongoose.connect(mongoURL);
 
+// mongoose.connect(mongoURL, {
+//     tlsAllowInvalidCertificates: true
+// });
+
 // Get the default connection
 // Mongoose maintains a default connection object representing the MongoDB connection
 const db = mongoose.connection;
@@ -18,13 +25,13 @@ const db = mongoose.connection;
 // Define event listeners for database connection
 
 db.on('connected', () => {
-    console.log('Connected to MongoDB server');
+    console.log('✅ Connected to MongoDB server');
 });
 
 db.on('error', (err) => {
-    console.log('MongoDB connection error', err);
+    console.log('❌ MongoDB connection error:', err);
 });
 
 db.on('disconnected', () => {
-    console.log('MongoDB disconnected');
+    console.log('⚠️ MongoDB disconnected');
 });
--- Hosting server in node js/server.js
+++ Hosting server in node js/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./db');  // MongoDB Connection
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());  // req body
+const PORT = process.env.PORT || 3000;
 
 // const Person = require('./models/Person');  // Person Mongoose models
 // const MenuItem = require('./models/MenuItem');  // MenuItem Mongoose models
@@ -120,6 +122,6 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening on port 3000');
 })
